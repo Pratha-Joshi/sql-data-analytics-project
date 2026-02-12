5da7659c1d57dc0824e8c59c8fb6925c953d4dfc
Pratha Joshi <prathaworking@gmail.com>
2026-02-12 16:12:36 +0530

    Update customers-report.sql

--- scripts/customers-report.sql
+++ scripts/customers-report.sql
@@ -7,7 +7,7 @@ Purpose:
 
 Highlights:
     1. Gathers essential fields such as names, ages, and transaction details.
-	  2. Segments customers into categories (VIP, Regular, New) and age groups.
+	2. Segments customers into categories (VIP, Regular, New) and age groups.
     3. Aggregates customer-level metrics:
 	   - total orders
 	   - total sales
